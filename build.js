1585991468bfe9054187fc5d63497ea25addd34c
ka2u-gc <h.13soccer.k@gmail.com>
2025-06-11 09:49:45 +0900

    Update build.js

--- build.js
+++ build.js
@@ -1,19 +1,30 @@
 // build.js
 const esbuild = require('esbuild');
+const fs = require('fs');
+const path = require('path');
 
-esbuild.build({
-  entryPoints: ['./src/utils.ts'],
-  bundle: true,
-  outfile: './dist/utils.js',
-  format: 'esm',
-  platform: 'browser',
-  target: ['es2022'],
-  sourcemap: false,
-  logLevel: 'info'
-}).catch(() => process.exit(1));
+const outdir = path.resolve(__dirname, 'dist');
+
+// dist ディレクトリがなければ作成
+if (!fs.existsSync(outdir)) {
+  fs.mkdirSync(outdir);
+  console.log('Created dist directory');
+}
+
+esbuild
+  .build({
+    entryPoints: ['./src/utils.ts'],
+    bundle: true,
+    outfile: './dist/utils.js',
+    format: 'esm',
+    platform: 'browser',
+    target: ['es2022'],
+    sourcemap: false,
+    logLevel: 'info',
+  })
+  .catch(() => process.exit(1));
 
 // build.js の最後に追加
-const fs = require('fs');
 fs.copyFileSync(
   'node_modules/onnxruntime-web/dist/ort-wasm-simd-threaded.jsep.wasm',
   'dist/ort-wasm-simd-threaded.jsep.wasm'
